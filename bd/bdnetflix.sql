de1d71a5b2f757ab77ff8d2242927dbb0e542eae
Bea Ruf <bearm12493@gmail.com>
2024-04-04 12:04:13 +0100

    select and more select

--- bd/bdnetflix.sql
+++ bd/bdnetflix.sql
@@ -23,3 +23,18 @@ lastName varchar(45) not null,
 country varchar(45) not null,
 birthday date not null
 );
+
+INSERT INTO movies (title, genre, image, category, yearMovie)
+VALUE ('Pulp Fiction', 'Crimen', 'https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg', 'Top 10', 1994),
+('La vita è bella', 'Comedia', 'https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg', 'Top 10', 1996),
+('Forrest Gump', 'Comedia', 'https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg', 'Top 10', 1994); 
+
+INSERT INTO users (user, password, name, email, plan_details)
+VALUE ('laura_dev', 'laura', 'Laura', 'laura@gmail.com', 'Standard'),
+('maria_dev', 'maria', 'Maria', 'maria@gmail.com', 'Standard'),
+('ester_dev', 'ester', 'Ester', 'ester@gmail.com', 'Standard');
+
+INSERT INTO actors (name, lastName, country, birthday)
+VALUE ('Tom', 'Hanks', 'Estados Unidos', '1956-07-09'),
+('Roberto', 'Benigni', 'Italia', '1956-10-27'),
+('John', 'Travolta', 'Estados Unidos', '1954-02-18');
