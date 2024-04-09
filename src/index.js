31633edc17e70ad174d98dbf82e69d140a5a9dc7
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-04-09 11:22:56 +0200

    add ne3w movies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require("mysql2/promise");
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
 
+
 // init express aplication
-const serverPort = 4000;
-server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
+async function getDBConnection() {
+  const connection = await mysql.createConnection({
+    host: "sql.freedb.tech",
+    user: "freedb_module_4_db",
+    password: "D&DSYcVsb3hz&q3",
+    database: "freedb_backend_movies",
+  });
+  connection.connect();
+  return connection;
+}
+
+const port = 4000;
+server.listen(port, () => {
+  console.log("Server is listening in http://localhost" + port);
+});
+
+server.get("/movies", async(req, res)=> {
+  const connection = await getDBConnection();
+  const sql = "SELECT * FROM movies";
+  const [moviesResult] = await connection.query(sql); 
+  connection.end();
+  res.status(200).json(
+    {
+      success: true,
+      movies:  moviesResult
+    }
+  ); 
 });
