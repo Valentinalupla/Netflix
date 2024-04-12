0a0218cc8816f394b417d9989d8e7143b1d05af8
Bea Ruf <bearm12493@gmail.com>
2024-04-12 10:33:38 +0100

    register but doesnt work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const mysql = require("mysql2/promise");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
-
 
 // init express aplication
 async function getDBConnection() {
@@ -22,25 +23,55 @@ async function getDBConnection() {
 
 const port = 4000;
 server.listen(port, () => {
-  console.log("Server is listening in http://localhost" + port);
+  console.log("Server is listening in http://localhost:" + port);
 });
 
-server.get("/movies", async(req, res)=> {
+//ENDPOINT MOVIES
+server.get("/movies", async (req, res) => {
   const connection = await getDBConnection();
   const genreFilterParam = req.query.genre;
   const sql = "SELECT * FROM movies WHERE genre= ?";
-  const [moviesResult] = await connection.query(sql, [genreFilterParam]); 
-  
+  const [moviesResult] = await connection.query(sql, [genreFilterParam]);
+
   console.log(req.query);
   connection.end();
-  res.status(200).json(
-    {
-      success: true,
-      movies:  moviesResult
-    }
-  ); 
+  res.status(200).json({
+    success: true,
+    movies: moviesResult,
+  });
 });
 
-server.get("/movies/:id", async(req, res)=> {
+server.get("/movies/:id", async (req, res) => {
   console.log(req.params.id);
-})
+});
+
+//ENDPOINTS USERS
+//signup
+
+server.post("/registro", async (req, res) => {
+  console.log("esto es el email y pass", req.body);
+
+  const { email, password } = req.body;
+  const connection = await getDBConnection();
+  const emailQuery = "SELECT * FROM users WHERE email = ?";
+  const [emailResult] = await connection.query(emailQuery, [email]);
+
+  // console.log("esto es algo", emailResult);
+  if (emailResult.length === 0) {
+    const passwordHashed = await bcrypt.hash(password, 10);
+    const newUserQuery = "INSERT INTO users (email, password) VALUES (?,?)";
+    const [newUserResults] = await connection.query(newUserQuery, [
+      email,
+      passwordHashed,
+    ]);
+    res.status(201).json({
+      success: true,
+      id: newUserResults.id,
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      message: "Email already registered",
+    });
+  }
+});
